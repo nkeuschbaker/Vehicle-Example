e72b9b7e71612816d2b796579acec012ab72d40e
nkeuschbaker <nkeuschbaker@friendsseminary.org>
2025-04-08 14:23:17 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,41 @@
+class Vehicle { // super class
+  public String brand = "Ford";
+  public void honk() {
+    System.out.println("Tuut, tuut!");
+  }
+}
+
+class Car extends Vehicle { // sub class
+  public String modelName;
+  
+  public Car(String modelName){
+  	this.modelName = modelName;
+  }
+  
+}
+
+class Train extends Vehicle{
+	public String line;
+    public Train(String line){
+    	this.line = line;
+    }
+    public void choo(){
+    	System.out.println("Choo Choo!!");
+    }
+}
+
+class Main{
+	public static void main(String[] args) {
+   		Vehicle v = new Vehicle();
+        v.honk();
+      Car myFastCar = new Car("Mustang");
+      myFastCar.honk();
+      System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+      Car otherCar = new Car("F150");
+      System.out.println("My other car is a " + otherCar.modelName);
+      Train train = new Train("1");
+      System.out.println(train.line);
+      train.choo();
+  }
+}
+
